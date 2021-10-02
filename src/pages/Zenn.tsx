dc06006ac521ea28a611b6e488c58b07786d821f
bpeldi2oerkd8 <takahashi208pwla4drb@gmail.com>
2021-10-02 23:42:33 +0900

    zennのページの編集

--- src/pages/Zenn.tsx
+++ src/pages/Zenn.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Box } from '@material-ui/core';
 
 const Zenn: React.FunctionComponent = () => {
-  return <h2>Zenn</h2>;
+  return(
+    <Box>
+      <h2>Zenn (Beta版)</h2>
+      <p>Zennから公式のAPIが提供されていないため、Beta版</p>
+    </Box>
+  );
 };
 
 export default Zenn;
