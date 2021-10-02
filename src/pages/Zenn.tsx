befff897bf8890e2c3480628769471aa692daaf3
bpeldi2oerkd8 <takahashi208pwla4drb@gmail.com>
2021-10-02 23:46:49 +0900

    コード整形

--- src/pages/Zenn.tsx
+++ src/pages/Zenn.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box } from '@material-ui/core';
 
 const Zenn: React.FunctionComponent = () => {
-  return(
+  return (
     <Box>
       <h2>Zenn (Beta版)</h2>
       <p>Zennから公式のAPIが提供されていないため、Beta版</p>
